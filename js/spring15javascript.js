ca2a4ff067d5346e3bd788f6fd66592d24f34678
Tysonium <tyson.daugherty@gmail.com>
2015-01-06 23:25:50 -0800

    responsive programs page

---
+++ js/spring15javascript.js
@@ -0,0 +1,219 @@
+
+window.jQuery(function ($) {
+    'use strict';
+    $("#spring15-javascript-submit").on('click', function (e) {
+			e.preventDefault();
+			console.log('student button working');
+			var firstName = $('#spring-student-first-name').val();
+			var lastName = $('#spring-student-last-name').val();
+			var email = $('#spring-student-email').val();
+			var cell = $('#spring-student-cell').val();
+			var school = $('#spring-student-school').val();
+			var grade = $('#spring-student-grade').val();
+			var gender = $('#spring-student-gender').val(); 
+			var ethnicity = $('#spring-student-ethnicity').val();
+			var comments = $('#spring-student-comments').val();
+
+			var check = new RegExp("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+			var isEmail = check.test(email);
+
+			if (!firstName) {
+				$('#spring-firstName-error-box').show();
+				$('#spring-student-first-name').addClass('spring15-input-error');
+				$('#spring-student-first-name').removeClass('spring15-input-success');
+			} else {
+				$('#spring-firstName-error-box').hide();
+				$('#spring-student-first-name').addClass('spring15-input-success');
+				$('#spring-student-first-name').removeClass('spring15-input-error');
+			}
+
+			if (!lastName) {
+				$('#spring-lastName-error-box').show();
+				$('#spring-student-last-name').addClass('spring15-input-error');
+				$('#spring-student-last-name').removeClass('spring15-input-success');
+			} else {
+				$('#spring-lastName-error-box').hide();
+				$('#spring-student-last-name').addClass('spring15-input-success');
+				$('#spring-student-last-name').removeClass('spring15-input-error');
+			}
+
+			if (!isEmail) {
+				$('#spring-email-error-box').show();
+				$('#spring-student-email').addClass('spring15-input-error');
+				$('#spring-student-email').removeClass('spring15-input-success');
+			} else {
+				$('#spring-email-error-box').hide();
+				$('#spring-student-email').addClass('spring15-input-success');
+				$('#spring-student-email').removeClass('spring15-input-error');
+			}
+
+			if (school === "school") {
+				$('#spring-school-error-box').show();
+				$('#spring-student-school').addClass('spring15-input-error');
+				$('#spring-student-school').removeClass('spring15-input-success');
+			} else {
+				$('#spring-school-error-box').hide();
+				$('#spring-student-school').addClass('spring15-input-success');
+				$('#spring-student-school').removeClass('spring15-input-error');
+			}
+
+			if (grade === "grade") {
+				$('#spring-grade-error-box').show();
+				$('#spring-student-grade').addClass('spring15-input-error');
+				$('#spring-student-grade').removeClass('spring15-input-success');
+			} else {
+				$('#spring-grade-error-box').hide();
+				$('#spring-student-grade').addClass('spring15-input-success');
+				$('#spring-student-grade').removeClass('spring15-input-error');
+			}
+
+			if (gender === "gender") {
+				$('#spring-gender-error-box').show();
+				$('#spring-student-gender').addClass('spring15-input-error');
+				$('#spring-student-gender').removeClass('spring15-input-success');
+			} else {
+				$('#spring-gender-error-box').hide();
+				$('#spring-student-gender').addClass('spring15-input-success');
+				$('#spring-student-gender').removeClass('spring15-input-error');
+			}
+
+			if (ethnicity === "ethnicity") {
+				$('#spring-ethnicity-error-box').show();
+				$('#spring-student-ethnicity').addClass('spring15-input-error');
+				$('#spring-student-ethnicity').removeClass('spring15-input-success');
+			} else {
+				$('#spring-ethnicity-error-box').hide();
+				$('#spring-student-ethnicity').addClass('spring15-input-success');
+				$('#spring-student-ethnicity').removeClass('spring15-input-error');
+			}
+
+			if (!firstName || !lastName || !isEmail || school === "school" || grade === "grade" || gender === "gender" || ethnicity === "ethnicity") {
+				$('#spring-submit-error-msg').show();
+			} else {
+				var Spring15Javascript = Parse.Object.extend("Spring15Javascript");
+				var spring15Javascript = new Spring15Javascript();
+				spring15Javascript.set("firstName", firstName);
+				spring15Javascript.set("lastName", lastName);
+				spring15Javascript.set("email", email);
+				spring15Javascript.set("cell", cell);
+				spring15Javascript.set("school", school);
+				spring15Javascript.set("grade", grade);
+				spring15Javascript.set("gender", gender);
+				spring15Javascript.set("ethnicity", ethnicity);
+				spring15Javascript.set("comments", comments);
+				spring15Javascript.set("class", "javascript");
+
+				spring15Javascript.save(null, {
+				  success: function(student) {
+				    console.log('save successful');
+				    $('#lab-student-form').hide();
+				    $('.spring-student-head-box').hide();
+				    $('.spring-student-success-box').show();
+				  },
+				  error: function(student, error) {
+				    console.log('Failed to create new object, with error code: ' + error.description);
+				  }
+				});
+				$('#lab-student-form').hide();
+			  $('.spring-student-head-box').hide();
+			  $('.spring-student-success-box').show();
+			}
+		})
+		$("#spring-student-first-name").on('focusout', function (e) {
+			e.preventDefault();
+			var firstName = $('#spring-student-first-name').val();
+			if (!firstName) {
+				$('#spring-student-first-name').addClass('spring15-input-error');
+				$('#spring-student-first-name').removeClass('spring15-input-success');
+			} else {
+				$('#spring-firstName-error-box').hide();
+				$('#spring-student-first-name').addClass('spring15-input-success');
+				$('#spring-student-first-name').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-last-name").on('focusout', function (e) {
+			e.preventDefault();
+			var lastName = $('#spring-student-last-name').val();
+			if (!lastName) {
+				$('#spring-student-last-name').addClass('spring15-input-error');
+				$('#spring-student-last-name').removeClass('spring15-input-success');
+			} else {
+				$('#spring-lastName-error-box').hide();
+				$('#spring-student-last-name').addClass('spring15-input-success');
+				$('#spring-student-last-name').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-email").on('focusout', function (e) {
+			e.preventDefault();
+			var email = $('#spring-student-email').val();
+			var check = new RegExp("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+			var isEmail = check.test(email);
+			if (!isEmail) {
+				$('#spring-student-email').addClass('spring15-input-error');
+				$('#spring-student-email').removeClass('spring15-input-success');
+			} else {
+				$('#spring-email-error-box').hide();
+				$('#spring-student-email').addClass('spring15-input-success');
+				$('#spring-student-email').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-school").on('focusout', function (e) {
+			e.preventDefault();
+			var school = $('#spring-student-school').val();
+			if (school === "school") {
+				$('#spring-student-school').addClass('spring15-input-error');
+				$('#spring-student-school').removeClass('spring15-input-success');
+			} else {
+				$('#spring-school-error-box').hide();
+				$('#spring-student-school').addClass('spring15-input-success');
+				$('#spring-student-school').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-grade").on('focusout', function (e) {
+			e.preventDefault();
+			var grade = $('#spring-student-grade').val();
+			if (grade === "grade") {
+				$('#spring-student-grade').addClass('spring15-input-error');
+				$('#spring-student-grade').removeClass('spring15-input-success');
+			} else {
+				$('#spring-grade-error-box').hide();
+				$('#spring-student-grade').addClass('spring15-input-success');
+				$('#spring-student-grade').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-gender").on('focusout', function (e) {
+			e.preventDefault();
+			var gender = $('#spring-student-gender').val();
+			if (gender === "gender") {
+				$('#spring-student-gender').addClass('spring15-input-error');
+				$('#spring-student-gender').removeClass('spring15-input-success');
+			} else {
+				$('#spring-gender-error-box').hide();
+				$('#spring-student-gender').addClass('spring15-input-success');
+				$('#spring-student-gender').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-ethnicity").on('focusout', function (e) {
+			e.preventDefault();
+			var ethnicity = $('#spring-student-ethnicity').val();
+			if (ethnicity === "ethnicity") {
+				$('#spring-student-ethnicity').addClass('spring15-input-error');
+				$('#spring-student-ethnicity').removeClass('spring15-input-success');
+			} else {
+				$('#spring-ethnicity-error-box').hide();
+				$('#spring-student-ethnicity').addClass('spring15-input-success');
+				$('#spring-student-ethnicity').removeClass('spring15-input-error');
+			}
+		})
+		$("#spring-student-comments").focus(function() {
+		    var $this = $(this);
+		    $this.select();
+		    // $this.mouseup(function() {
+		    //     $this.unbind("mouseup");
+		    //     return false;
+		    // });
+		});
+  })
+
+
+
