25e08166233bd0d74b2f51b09b63d91eaf2dbeea
Tysonium <tyson.daugherty@gmail.com>
2015-01-08 12:06:36 -0800

    lab mentor form updates

--- js/hackerlab.js
+++ js/hackerlab.js
@@ -3,48 +3,46 @@ window.jQuery(function ($) {
     'use strict';
     $("#hack-mentor-submit").on('click', function (e) {
 			e.preventDefault();
-			console.log('mentor button working');
+
 			var days = [];
 			var time = [];
 			var firstName = $('#hack-mentor-first-name').val();
 			var lastName = $('#hack-mentor-last-name').val();
 			var email = $('#hack-mentor-email').val();
+			var frequency = $('#hack-mentor-freq').val();
 
-			if($('#morning').is(':checked')) {
-		    var morning = $('#morning').val();
-		    console.log(morning);
-		    time.push(morning);
-		  }
+			// if($('#morning').is(':checked')) {
+		 //    var morning = $('#morning').val();
+		 //    console.log(morning);
+		 //    time.push(morning);
+		 //  }
 		  if($('#afternoon').is(':checked')) {
 		    var afternoon = $('#afternoon').val();
-		    console.log(afternoon);
 		    time.push(afternoon);
 		  }
 		  if($('#evening').is(':checked')) {
 		    var evening = $('#evening').val();
-		    console.log(evening);
 		    time.push(evening);
 		  }
 			if($('#monday').is(':checked')) {
 		    var availMonday = $('#monday').val();
-		    console.log(availMonday);
 		    days.push(availMonday);
 		  }
 		  if($('#tuesday').is(':checked')) {
 		    var availTuesday = $('#tuesday').val();
-		    console.log(availTuesday);
 		    days.push(availTuesday);
 		  }
 		  if($('#wednesday').is(':checked')) {
 		    var wednesday = $('#wednesday').val();
-		    console.log(wednesday);
 		    days.push(wednesday);
 		  }
 		  if($('#thursday').is(':checked')) {
 		    var thursday = $('#thursday').val();
-		    console.log(thursday);
 		    days.push(thursday);
 		  }
+
+		  // console.log(time);
+		  // console.log(days);
 
 		  var LabMentor = Parse.Object.extend("LabMentor");
 			var labMentor = new LabMentor();
@@ -53,7 +51,8 @@ window.jQuery(function ($) {
 			labMentor.set("email", email);
 			labMentor.set("time", time);
 			labMentor.set("days", days);
-			console.log(days + '  ' + time + '  ' + firstName + '  ' + lastName + '  ' + email);
+			labMentor.set("frequency", frequency);
+			// console.log(days + '  ' + time + '  ' + firstName + '  ' + lastName + '  ' + email);
 
 			labMentor.save(null, {
 			  success: function(mentor) {
