feefafa3f32d4020ab995cb8bbbc579a13616448
Bob Ippolito <bob@redivi.com>
2017-09-11 11:25:02 -0700

    Remove references to Parse from main site JS

--- js/site.js
+++ js/site.js
@@ -8,5 +8,4 @@ window.jQuery(function ($) {
     if ($.fn.qrcode !== undefined) {
         $('.doge-qrcode').qrcode($('.doge-qrcode').data('qrcode'));
     }
-    Parse.initialize("xo38yYms5wCczcXUDt8NEld33H98ZIAtiNZefxqs", "dlgmY1RTvVKxa7N7pT83RNrVzrc0CryKTwujA590");
 });
