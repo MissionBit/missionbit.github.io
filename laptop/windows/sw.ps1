aa07a4dfdbfcbe7422c3ee9bbc24bed3c659b9b8
juemura <ju.uemura@gmail.com>
2018-08-21 17:02:22 -0700

    fixes to windows script

--- laptop/windows/sw.ps1
+++ laptop/windows/sw.ps1
@@ -1,7 +1,8 @@
 # Run Chocolatey script
 cinst -y $( ( [xml]( Invoke-WebRequest -Uri https://www.missionbit.com/laptop/windows/mb.config -UseBasicParsing) ).packages.package | Select id ).id
 # Download Meraki agent installer
-#(New-Object System.Net.WebClient).DownloadFile("https://www.missionbit.com/laptop/windows/meraki-agent-missionbit.1.0.nupkg", "C:\Windows\Temp\archive.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://www.missionbit.com/laptop/windows/meraki-agent-missionbit.1.0.nupkg")
+(New-Object System.Net.WebClient).DownloadFile("https://www.missionbit.com/laptop/windows/meraki-agent-missionbit.1.0.nupkg", "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg")
 # Install Meraki agent
-cinst meraki-agent-missionbit -s .
+cinst meraki-agent-missionbit -s "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg" -y
+# Delete installer
+Remove-Item -path C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg -recurse
