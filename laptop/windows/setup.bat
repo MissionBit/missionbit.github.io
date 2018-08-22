da47bbc81351e1b355e66862a59e20c5c43c79c2
juemura <ju.uemura@gmail.com>
2018-08-22 15:45:19 -0700

    simplified windows script and fixed instructions

--- laptop/windows/setup.bat
+++
@@ -1,5 +0,0 @@
-:: This batch file installs Chocolatey and runs a PowerShell script to set up a new MB laptop
-:: Install Chocolatey
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-:: Run PowerShell script as administrator
-powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://www.missionbit.com/laptop/windows/sw.ps1'))"
--- laptop/windows/setup.ps1
+++ laptop/windows/setup.ps1
@@ -1,10 +1,10 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# Run Chocolatey script
-cinst -y $( ( [xml]( Invoke-WebRequest -Uri https://www.missionbit.com/laptop/windows/mb.config -UseBasicParsing) ).packages.package | Select id ).id
 # Download Meraki agent installer
 (New-Object System.Net.WebClient).DownloadFile("https://www.missionbit.com/laptop/windows/meraki-agent-missionbit.1.0.nupkg", "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg")
 # Install Meraki agent
 cinst meraki-agent-missionbit -s "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg" -y
 # Delete installer
 Remove-Item -path C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg -recurse
+# Run Chocolatey script
+cinst -y $( ( [xml]( Invoke-WebRequest -Uri https://www.missionbit.com/laptop/windows/mb.config -UseBasicParsing) ).packages.package | Select id ).id
