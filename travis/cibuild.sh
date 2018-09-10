e5809f89920648182879395b3528dba1020903a7
Bob Ippolito <bob@redivi.com>
2018-09-10 13:02:54 -0700

    Fix invalid HTML and ignore external requests that were aborted

--- travis/cibuild.sh
+++ travis/cibuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --allow-hash-href --empty-alt-ignore
+bundle exec htmlproofer ./_site --allow-hash-href --empty-alt-ignore --http-status-ignore="0"
