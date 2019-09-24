b6f9cb7fdf7485dac17dc0116a8bad3e7655ff0e
Bob Ippolito <bob@redivi.com>
2019-09-24 16:17:34 -0700

    Remove temporary ignore of drive links

--- ci/cibuild.sh
+++ ci/cibuild.sh
@@ -2,9 +2,7 @@
 set -e
 DESTINATION="${DESTINATION:-./_site}"
 bundle exec jekyll build -d "${DESTINATION}"
-# Temporarily ignore drive.google.com broken links
 bundle exec htmlproofer "${DESTINATION}" \
     --allow-hash-href \
     --empty-alt-ignore \
-    --url-ignore "/drive.google.com/" \
     --http-status-ignore="0,403"
