588a6928bb680b92b79af9b9f55a38e7d679fd49
Bob Ippolito <bob@redivi.com>
2020-01-31 07:16:54 -0800

    Update missionbit.com to missionbit.org

--- laptop/windows/setup.ps1
+++ laptop/windows/setup.ps1
@@ -3,10 +3,10 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 # Refresh / Update Session Environment
 refreshenv
 # Download Meraki agent installer
-(New-Object System.Net.WebClient).DownloadFile("https://www.missionbit.com/laptop/windows/meraki-agent-missionbit.1.0.nupkg", "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg")
+(New-Object System.Net.WebClient).DownloadFile("https://www.missionbit.org/laptop/windows/meraki-agent-missionbit.1.0.nupkg", "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg")
 # Install Meraki agent
 cinst meraki-agent-missionbit -s "C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg" -y
 # Delete installer
 Remove-Item -path C:\users\missionbit\downloads\meraki-agent-missionbit.1.0.nupkg -recurse
 # Run Chocolatey script
-cinst -y $( ( [xml]( Invoke-WebRequest -Uri https://www.missionbit.com/laptop/windows/mb.config -UseBasicParsing) ).packages.package | Select id ).id
+cinst -y $( ( [xml]( Invoke-WebRequest -Uri https://www.missionbit.org/laptop/windows/mb.config -UseBasicParsing) ).packages.package | Select id ).id
