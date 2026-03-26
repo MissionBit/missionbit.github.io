e198d01c28a3b6f766a566ae35d1e16f628b5a40
Bob Ippolito <bob@redivi.com>
2026-03-26 07:39:09 -0700

    disable htmlproofer

--- ci/cibuild.sh
+++ ci/cibuild.sh
@@ -3,9 +3,9 @@ set -e
 DESTINATION="${DESTINATION:-./_site}"
 jekyll build -d "${DESTINATION}" --trace
 # Temporarily ignore drive.google.com broken links
-htmlproofer "${DESTINATION}" \
-    --allow-hash-href \
-    --empty-alt-ignore \
-    --url-ignore "/drive.google.com/" \
-    --http-status-ignore="0,403" \
-    --trace
+# htmlproofer "${DESTINATION}" \
+#     --allow-hash-href \
+#     --empty-alt-ignore \
+#     --url-ignore "/drive.google.com/" \
+#     --http-status-ignore="0,403" \
+#     --trace
